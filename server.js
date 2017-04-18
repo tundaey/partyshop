add5c97e304a1bc0c841666d4454c1f4eb8df216
Tunde <tundaey@gmail.com>
2017-04-18 13:25:59 +0100

    route all requests to angular

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
