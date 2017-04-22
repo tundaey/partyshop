088554b524c43d3ed16e34a2246fa2e52afc16bd
Tunde <tundaey@gmail.com>
2017-04-22 20:30:48 +0100

    fixed 404 refresh error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
